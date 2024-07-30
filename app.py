755441c751a6295b3877b7528fb561d593ae9ced
Noam Avned <naomavned@gmail.com>
2024-07-30 18:23:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,62 @@
 from tkinter import *
+import re
+import datetime
+from github import Github
+from github import Auth
+from tkinter import messagebox
 
+
+def createTitle() -> str:
+    """Create GitHub title
+
+    Returns:
+        str: title
+    """
+    t = datetime.datetime.now()
+    d = t.strftime("%d")
+    m = t.strftime("%m")
+    if m[0] == "0":
+        m = m[1]
+    if d[0] == "0":
+        d = d[1]
+    title = f'{d}-{m}-{t.strftime("%Y")}.html'
+    return title
+
+def sort_file_paths_by_date(file_paths) -> list:
+    def extract_date(file_path) -> list:
+        date_part = file_path.split('.')[0]
+        d = date_part.split('-')
+        if len(d[0]) == 1:
+            d[0] = "0"+d[0]
+        if len(d[1]) == 1:
+            d[1] = "0"+d[1]
+        date_part = '-'.join(d)
+        return datetime.datetime.strptime(date_part, "%d-%m-%Y")
+    sorted_file_paths = sorted(file_paths, key=extract_date)
+    return sorted_file_paths
+
+def publish(t: str) -> None:
+    title = createTitle()
+    auth = Auth.Token("Your TOKEN")
+    g = Github(auth=auth)
+    repo = g.get_repo(824887772)
+    l = 0
+    conts = []
+    contents = repo.get_contents("")
+    while len(contents) > 1:
+        l += 1
+        file_content = contents.pop(0)
+        conts.append(file_content)
+        if file_content.type == "dir":
+            contents.extend(repo.get_contents(file_content.path))
+    contents = sort_file_paths_by_date(list(map(lambda x: x.path, conts)))
+    if l >= 50:
+        l -= 49
+        for i in range(l):
+            file_content = repo.get_contents(contents[i])
+            repo.delete_file(contents[i], f"Delete {contents[i]}", file_content.sha)
+    repo.create_file(title, f"Create {title}", t)
+    messagebox.showinfo("Done", f"Deleted {l} files.\nAdded 1 file")
 
 def copy(rt: Tk) -> None:
     """Copy content of element
@@ -12,17 +69,16 @@ def copy(rt: Tk) -> None:
     rt.update()
     rt.after(100, rt.update())
 
-def insertDisabled(element: Text, index: int, text: str) -> None:
-    """Insert into a disabled element
+def insertTxt(element: Text, index: int, text: str) -> None:
+    """Insert into an element
 
     Args:
         element (Text): Text element to insert into
         index (int): index to insert to
         text (str): text to insert
     """
-    element.config(state=NORMAL)
+    element.delete('1.0', END)
     element.insert(index, text)
-    element.config(state=DISABLED)
 
 def insertStr(s: str, index: int, t: str) -> str:
     """Insert string into string in index
@@ -48,6 +104,9 @@ def generate(element: Text, element2: Text) -> None:
     styleloc = txt.index("<style>")
     txt = insertStr(txt, styleloc, '\n')
     txt = insertStr(txt, styleloc + 8, '\n\t')
+    lst = [m.end() for m in re.finditer('solid', txt)]
+    for i in lst:
+        txt = insertStr(txt, i, " ")
 
     formatted_txt = ""
     for char in txt:
@@ -64,7 +123,7 @@ def generate(element: Text, element2: Text) -> None:
         else:
             formatted_txt += char
 
-    insertDisabled(element2, 1.0, formatted_txt)
+    insertTxt(element2, 1.0, formatted_txt)
 
 
 # initialize root
@@ -72,21 +131,23 @@ root = Tk()
 root.title("Html Formatter")
 
 # input elements
-lblinp = Label(root, text="input")
-lblinp.grid()
+lblinp = Label(root, text="input", background="grey", foreground="white")
+lblinp.grid(columnspan=1, sticky="nsew")
 txtarea = Text(root, height=20, width=70)
 txtarea.grid(row=1, column=0)
 
 # output elements
-lblout = Label(root, text="output")
-lblout.grid(row=0, column=1)
-txtarea2 = Text(root, height=20, width=70, state=DISABLED)
+lblout = Label(root, text="output", background="grey", foreground="white")
+lblout.grid(row=0, column=1, columnspan=3, sticky="nsew")
+txtarea2 = Text(root, height=20, width=70)
 txtarea2.grid(row=1, column=1)
 
 # buttons
-copybtn = Button(root, text="generate", command=lambda: generate(txtarea, txtarea2))
-copybtn.grid(row=2, column=0)
-copybtn = Button(root, text="copy", command=lambda: copy(root))
-copybtn.grid(row=2, column=1)
+genbtn = Button(root, text="generate", background="grey", foreground="white", command=lambda: generate(txtarea, txtarea2))
+genbtn.grid(row=2, column=0, columnspan=1, sticky="nsew")
+copybtn = Button(root, text="copy", background="grey", foreground="white", command=lambda: copy(root))
+copybtn.grid(row=2, column=1, columnspan=3, sticky="nsew")
+publishbtn = Button(root, text="publish", background="grey", foreground="white", command=lambda: publish(txtarea2.get(1.0, END)))
+publishbtn.grid(row=3, column=0, columnspan=3, sticky="nsew")
 
 root.mainloop()
